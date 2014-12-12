85d2e02112539298171a6532b20857263e5620f2
Denny Riadi <driadi@liveops.com>
2014-12-12 22:33:56 +1300

    Checked in missing env configuration files in config directory

---
+++ config/env/development.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db: 'mongodb://localhost/meanproduct',
+    runningPort: 3000
+};
---
+++ config/env/test.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db: 'mongodb://localhost/meanproduct-test',
+    runningPort: 3001
+};
