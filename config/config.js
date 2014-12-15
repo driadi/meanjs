23adebc115f1ad23a6d32830d5d4e621c5e077b3
Denny Riadi <driadi@liveops.com>
2014-12-15 13:52:03 +1300

    Set default process env as development

--- config/config.js
+++ config/config.js
@@ -1,3 +1,7 @@
+if (typeof process.env.NODE_ENV === 'undefined') {
+    process.env.NODE_ENV = 'development';
+}
+
 var config = require('./env/' + process.env.NODE_ENV) || {};
 
 module.exports = config;
